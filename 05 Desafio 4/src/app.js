488564566e414cc68b6ce05adbd3e8a72be8362a
luisliquin <luisliquin@outlook.com.ar>
2024-03-12 11:43:28 -0300

    view websockets

--- 05 Desafio 4/src/app.js
+++ 05 Desafio 4/src/app.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+import __dirname from "./utils.js";
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
 
+//Declaracion de constantes
 const port = 8080;
 const app = express(); 
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+//motor de plantillas
+app.engine("handlebars", handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views`);
+
+//Servidor estatico de archivos
+app.use(express.static(`${__dirname}/../public`)); 
 
 //Test de ingreso
 app.get('/api', (req, res) => {
