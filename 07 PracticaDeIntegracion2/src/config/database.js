e1139246e32bef2f4da417434061a046f6889549
luisliquin <luisliquin@outlook.com.ar>
2024-04-28 02:32:17 -0300

    pass

--- 07 PracticaDeIntegracion2/src/config/database.js
+++ 07 PracticaDeIntegracion2/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect("mongodb+srv://luisliquin:5VdRQt7U9jhvswU4@cluster0.e7prtgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {dbName: "ecommerce"})
+        await mongoose.connect("mongodb+srv://<user>:<pass>@cluster0.e7prtgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {dbName: "ecommerce"})
         console.log("conectado a la bbdd en mongo")
     } catch (error) {
         console.log("fallo conexion")
