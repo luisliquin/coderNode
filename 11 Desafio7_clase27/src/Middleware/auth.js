51b368779cb622b03a9209f8e5e9f0e04664c847
luisliquin <luisliquin@outlook.com.ar>
2024-06-17 17:16:47 -0300

    modificacio refactorizacion

--- 11 Desafio7_clase27/src/Middleware/auth.js
+++
@@ -1,8 +0,0 @@
-export const auth = function (req, res, next) {
-    console.log("Session:", req.session);  
-    if (!req.session || !req.session.user) {
-        console.log("No se encontro un usuario en la sesion, se redigira al login");
-        return res.redirect("/login");
-    }
-    return next();
-}
--- 11 Desafio7_clase27/src/config/handlebars.js
+++ 11 Desafio7_clase27/src/config/handlebars.js
@@ -1,10 +1,14 @@
 import handlebars from 'express-handlebars';
-import __dirname from '../utils/utils.js';
+import {__dirname} from '../utils.js';
 
 const setupHandlebars = (app) => {
-    app.engine("handlebars", handlebars.engine());
-    app.set("views", `${__dirname}/views`);
-    app.set("view engine", "handlebars");
+    try {
+        app.engine("handlebars", handlebars.engine());
+        app.set("views", `${__dirname}/views`);
+        app.set("view engine", "handlebars");        
+    } catch (error) {
+        console.log(`Error en handlebars.js: ${error}`)
+    }
 };
 
 export default setupHandlebars;
